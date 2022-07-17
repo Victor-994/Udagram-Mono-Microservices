c29b3472b903bf22a3f24939dba02c8d04c71dc5
Victor-994 <victoradejumo2@gmail.com>
2022-07-17 14:32:39 +0100

    new screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae20588651f674a1db8a1324e92531d1-939326557.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
